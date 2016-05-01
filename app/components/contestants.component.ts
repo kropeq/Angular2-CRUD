be459d8aea27d80428bcfa90decf7eab5856455f
Michal Jaworowski <kropeq88@gmail.com>
2016-05-01 11:55:42 +0200

    Stworzenie stylu dla listy użytkowników ( tabelka ) + dodanie eventu, który odczytuje kliknięty element z tabeli

--- app/components/contestants.component.ts
+++ app/components/contestants.component.ts
@@ -8,18 +8,34 @@ import {UserService} from '../services/user.service';
 	selector: 'my-contestants',
 	template: `
 		<div id="contestants">
-			<div>
-				<div *ngFor="#user of users">
-					<div> {{user.username}} {{user.password}} </div>
+			<div class="table">
+				<div class="singleUser">
+					<div class="cell">Name</div>
+					<div class="cell">Pass</div>
+				</div>
+				<div class="singleUser" *ngFor="#user of users" (click)="FillOnClick(user.username)">
+					<div class="cell">{{user.username}}</div>
+					<div class="cell">{{user.password}}</div>
 				</div>
 			</div>
+			<div *ngIf="currentUser">{{currentUser}}</div>
 		</div>
 	`
 })
 
 export class ContestantsComponent { 
 	//users : User[] = [];
+
+	currentUser = "";
 	constructor(private userService: UserService) { }
 	users = this.userService.getUsers();
+
+	FillOnClick(user){
+		if(this.currentUser === user ){
+			this.currentUser = '';
+			return;
+		} 
+		this.currentUser = user;
+	}
 
 }
